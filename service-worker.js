46df2888d2e24e97c98fc14e218ab4df10b7fe73
marcelocrediti <marcelinoteixeira.santos@gmail.com>
2026-09-16 07:35:03 -0300

    Atualiza cache das patrulhas especializadas

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ============================================================
 // Service Worker — Sistema de Ocorrências, Guarda Municipal de Itapajé
 // ============================================================
-const CACHE_NAME = 'gm-itapaje-app-v15';
+const CACHE_NAME = 'gm-itapaje-app-v16';
 
 const ASSETS_TO_CACHE = [
   './',
